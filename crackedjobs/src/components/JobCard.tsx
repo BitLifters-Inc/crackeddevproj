1b7b0ff95aec90bd89e8e0f600bd55fd841d3d6e
Eric Yun <yunseric@gmail.com>
2024-03-19 00:01:13 -0700

    fixed type erros in JobCard component

--- crackedjobs/src/components/JobCard.tsx
+++ crackedjobs/src/components/JobCard.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { type jobType } from '~/lib/utils'
+'use client'
+import React from 'react';
+import { type jobType } from '~/lib/types';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
 
 
 
-const JobCard = (props: jobType) => {
+const JobCard: React.FC<jobType> = (job: jobType) => {
+
   return (
-    <div>JobCard</div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <h1>
+            {job.title} 
+          </h1>
+        </CardTitle>
+
+      </CardHeader>
+    </Card>
+  );
+};
+
 
 export default JobCard
