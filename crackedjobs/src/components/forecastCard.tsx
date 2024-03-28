fa765d1c8d0172de0b9a0a73815dfb4e5377559f
Sarah Parc <sarahparc@gmail.com>
2024-03-27 20:25:20 -0700

    cleaned up forecast card, adjusted font sizing to better fit screen

--- crackedjobs/src/components/forecastCard.tsx
+++ crackedjobs/src/components/forecastCard.tsx
@@ -3,6 +3,7 @@ import { SubCard } from "./subCard";
 import { Combobox } from "./ui/dropdown";
 import { jobType } from "~/lib/types";
 import '../../../crackedjobs/src/styles/chart.css'
+import '../styles/globals.css';
 
 // interface ForecastCardProps {
 //   jobPostingsToday: jobType[];
@@ -39,12 +40,12 @@ import '../../../crackedjobs/src/styles/chart.css'
 const ForecastCard: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-3/4 bg-blue-300 opacity-95 shadow-lg rounded-md">
-        <div className="flex items-center space-x-4 p-1 justify-center" style={{ fontFamily: 'PokemonFRLG, sans-serif' , paddingTop: "50px" }}>
-          <h1 className="font-bold text-4xl text-white">CrackedDev Job Forecast</h1>
+      <div className="w-3/4 bg-blue-400 shadow-lg rounded-md" style={{ opacity: 0.85 }}>
+        <div className="flex items-center space-x-4 p-1 justify-center" style={{ fontFamily: 'PokemonFRLG, sans-serif', paddingTop: "50px" }}>
+          <h1 className="font-bold text-4xl text-white" style={{ fontSize: '5rem' }}>CrackedDev Job Forecast</h1>
           <Combobox />
         </div>
-        <div className="grid grid-cols-5 gap-4 p-5" style={{ fontFamily: 'PokemonFRLG, sans-serif' }} >
+        <div className="grid grid-cols-5 gap-3 p-6" style={{ fontFamily: 'PokemonFRLG, sans-serif' }} >
           <SubCard title="React" description="React looking grim" />
           <SubCard title="Angular" description="Angular forecast" />
           <SubCard title="Vue" description="Vue forecast" />
@@ -57,4 +58,3 @@ const ForecastCard: React.FC = () => {
 };
 
 export default ForecastCard;
-
